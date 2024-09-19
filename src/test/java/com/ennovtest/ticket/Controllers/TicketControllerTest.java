2c452265ee5f819b2325977ab76415e2d30503dc
estheranhe <estheranhe@consultech.ci>
2024-09-19 18:19:48 +0000

    TicketControllerTest

---
+++ src/test/java/com/ennovtest/ticket/Controllers/TicketControllerTest.java
@@ -0,0 +1,52 @@
+package com.ennovtest.ticket.Controllers;
+
+import com.ennovtest.ticket.Entities.Ticket;
+import com.ennovtest.ticket.Entities.TicketStatus;
+import com.ennovtest.ticket.Services.TicketService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.mockito.Mockito.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@WebMvcTest(TicketController.class)
+public class TicketControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TicketService ticketService;
+
+    @Test
+    public void getTickets() throws Exception {
+        List<Ticket> tickets = new ArrayList<>();
+        Ticket ticketA = new Ticket();
+        ticketA.setTitle("Test Ticket A");
+        ticketA.setDescription("Ceci est le ticket test A");
+        ticketA.setTicketStatus(TicketStatus.IN_PROGRESS);
+
+        Ticket ticketB = new Ticket();
+        ticketB.setTitle("Test Ticket B");
+        ticketB.setDescription("Ceci est le ticket test B");
+        ticketB.setTicketStatus(TicketStatus.CANCELLED);
+
+        tickets.add(ticketA);
+        tickets.add(ticketB);
+
+        when(ticketService.findAll()).thenReturn(tickets);
+
+        mockMvc.perform(get("/tickets"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].title()").value("Test Ticket A"));
+    }
+}
--- src/test/java/com/ennovtest/ticket/services/TicketServiceTest.java
+++ src/test/java/com/ennovtest/ticket/services/TicketServiceTest.java
@@ -3,6 +3,7 @@ package com.ennovtest.ticket.services;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import com.ennovtest.ticket.Entities.Ticket;
+import com.ennovtest.ticket.Entities.TicketStatus;
 import com.ennovtest.ticket.Repositories.TicketRepository;
 import com.ennovtest.ticket.Services.TicketService;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -34,5 +38,30 @@ public class TicketServiceTest {
         assertNotNull(createdTicket);
         assertEquals("Ticket Test 1", createdTicket.getTitle());
         verify(ticketRepository, times(1)).save(ticket);
+    }
+
+    @Test
+    public void testFindTickets(){
+        List<Ticket> tickets = new ArrayList<>();
+        Ticket ticketA = new Ticket();
+        ticketA.setTitle("Test Ticket A");
+        ticketA.setDescription("Ceci est le ticket test A");
+        ticketA.setTicketStatus(TicketStatus.IN_PROGRESS);
+
+        Ticket ticketB = new Ticket();
+        ticketB.setTitle("Test Ticket B");
+        ticketB.setDescription("Ceci est le ticket test B");
+        ticketB.setTicketStatus(TicketStatus.IN_PROGRESS);
+
+        tickets.add(ticketA);
+        tickets.add(ticketB);
+
+        when(ticketRepository.findAll()).thenReturn(tickets);
+
+        List<Ticket> result = ticketService.findAll();
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        verify(ticketRepository, times(1)).findAll();
     }
 }
